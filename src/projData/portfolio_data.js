350514deaf2aa7de1f9feca1501af73ee5610f22
Adi Bev <adityabev@gmail.com>
2019-02-24 15:30:14 +0000

    folder contains project datas

---
+++ src/projData/portfolio_data.js
@@ -0,0 +1,43 @@
+export const projectData = [
+  {
+    portflio_data: [
+      {
+        data: {
+          proj_title: "WeatherSense",
+          proj_desc:
+            "A weather app built using React and Styled-components with CSS Grid layout.",
+          proj_url: "https://weathersensepro.firebaseapp.com/",
+          github_url: "https://github.com/AdiBev/WeatherSense"
+        }
+      },
+
+      {
+        data: {
+          proj_title: "YumRecipes",
+          proj_desc:
+            "A Recipe finder app built with React, React-router, Redux and Bootstrap4.",
+          proj_url: "https://recipe-finder-007.firebaseapp.com/",
+          github_url: "https://github.com/AdiBev/YUMRecipes"
+        }
+      },
+
+      {
+        data: {
+          proj_title: "React-boilerplate",
+          proj_desc: "This is a boilerplate for React-Babel-Webpack projects.",
+          proj_url: false,
+          github_url: "https://github.com/AdiBev/react-boilerplate"
+        }
+      },
+      {
+        data: {
+          proj_title: "Portfolio",
+          proj_desc:
+            "Personal portfolio built using CSS grid, flexbox and React.",
+          proj_url: "https://adi-portfolio.firebaseapp.com/",
+          github_url: "https://github.com/AdiBev/Portfolio"
+        }
+      }
+    ]
+  }
+];
