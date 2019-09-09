18744b127ffa1a3a9fb0356ace2775054a27022a
Adi Bev <adityabev@gmail.com>
2019-09-09 20:09:54 +0100

    New projects added Pokedex: client and server

--- src/projData/portfolio_data.js
+++ src/projData/portfolio_data.js
@@ -31,11 +31,38 @@ export const projectData = [
       },
       {
         data: {
+          proj_title: "Ecommerce shop",
+          proj_desc:
+            "I built this shopping cart app using React, Sass and for layout I used flexbox.",
+          proj_url: "https://ecommerceshop-3294c.firebaseapp.com/",
+          github_url: "https://github.com/AdiBev/Ecommerceshop-app"
+        }
+      },
+      {
+        data: {
           proj_title: "Portfolio",
           proj_desc:
             "This my older version of portfolio built using CSS grid, flexbox and React.",
           proj_url: "https://adi-portfolio.firebaseapp.com/",
           github_url: "https://github.com/AdiBev/Portfolio"
+        }
+      },
+      {
+        data: {
+          proj_title: "Pokedex CRUD app",
+          proj_desc:
+            "A simple Pokedex CRUD app built using React, Redux, ANT.desgin",
+          proj_url: "http://pokedex-crudapp.herokuapp.com/",
+          github_url: "https://github.com/AdiBev/pokedex-crudapp"
+        }
+      },
+      {
+        data: {
+          proj_title: "Pokedex CRUD app server",
+          proj_desc:
+            "Server built with node.js and Express.js, MongoDB for the Pokedex CRUD app",
+          proj_url: "https://pokemon-api-server.herokuapp.com/",
+          github_url: "https://github.com/AdiBev/crudapp-server"
         }
       }
     ]
